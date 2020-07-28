a084001297d8d7a8f80a5bfd6dc5bc9683d3e730
Luis <luivasfer@gmail.com>
2020-07-28 18:33:23 -0400

    css portada inicio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    purge: [],
+    theme: {
+        fontFamily: {
+
+            rale: ['Poppins']
+        },
+        extend: {
+            colors: {
+                danger: '#e8505b',
+                info: {
+                    100: '#f09ae9',
+                    200: '#ffc1fa',
+                }
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
